159002ff5045f38bba9b37d7558b5ae3cf6e8768
Daniel Chen <cchen086@usc.edu>
2020-12-11 08:40:00 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("2ea352ed-d571-4b27-948a-19a9338b3d50");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://23.96.51.239:3001";
 
